56e04188ee463127aaadec8074b042e5ada4a002
Robotcider <tonyortiz-18@hotmail.com>
2014-09-01 15:08:35 -0700

    Fucking placeHolder Man! who knows how many years it will take me to finish even one game

--- WeakSven/WeakSven/Character.cs
+++ WeakSven/WeakSven/Character.cs
@@ -6,8 +6,6 @@ namespace WeakSven
 {
 	class Character : Entity
 	{
-		public Animation animation = new Animation();
-
 		protected Rectangle rect = new Rectangle(0, 0, 0, 0);
 
 		public Vector2 Position { get; set; }
@@ -19,16 +17,10 @@ namespace WeakSven
 
 		public virtual void Load(ContentManager Content, string imageFile)
 		{
-			animation.FrameCountX = 4;
-			animation.FrameCountY = 4;
-			animation.FramesPerSec = 33;
 
-			animation.SpriteSheet = Content.Load<Texture2D>(imageFile);
 
 			rect.X = (int)Position.X;
 			rect.Y = (int)Position.Y;
-			rect.Width = animation.FrameWidth;
-			rect.Height = animation.FrameHeight;
 		}
 
 		public virtual void Update(GameTime gameTime)
@@ -37,21 +29,11 @@ namespace WeakSven
 
 			rect.X = (int)Position.X;
 			rect.Y = (int)Position.Y;
-
-			if (Velocity == Vector2.Zero)
-			{
-				animation.Frame = 1;
-				animation.Paused = true;
-			}
-			else
-				animation.Paused = false;
-
-			animation.Update(gameTime);
 		}
 
 		public void Draw(SpriteBatch spriteBatch)
 		{
-			animation.Draw(spriteBatch, Position);
+
 		}
 	}
 }
